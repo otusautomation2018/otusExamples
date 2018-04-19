c143cc97bf85683599772505b9e45a085dda5a69
Anton <toshidono.it.work@gmail.com>
2018-04-19 20:17:30 +0300

    create stub test

---
+++ src/test/java/tests/lesson05/Lesson05Test.java
@@ -0,0 +1,11 @@
+package tests.lesson05;
+
+import org.testng.annotations.Test;
+
+public class Lesson05Test {
+
+    @Test
+    public void test() {
+        System.out.println("The property: ");
+    }
+}

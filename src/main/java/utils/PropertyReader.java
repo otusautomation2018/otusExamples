cab4d16eb8b24b6ee095d51f8cfe293fe29dd839
Anton <toshidono.it.work@gmail.com>
2018-04-21 20:38:03 +0300

    add test how use property

---
+++ src/main/java/utils/PropertyReader.java
@@ -0,0 +1,40 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    public static String getPropertyFromFile(String pathToFile, String propertyName) {
+        Properties prop = new Properties();
+        InputStream input = null;
+        String propertyValue = null;
+
+        try {
+
+            input = new FileInputStream(pathToFile);
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value
+            propertyValue = prop.getProperty(propertyName);
+            return propertyValue;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return propertyValue;
+    }
+}
--- src/test/java/tests/lesson05/Lesson05Test.java
+++ src/test/java/tests/lesson05/Lesson05Test.java
@@ -1,11 +1,13 @@
 package tests.lesson05;
 
 import org.testng.annotations.Test;
+import utils.PropertyReader;
 
 public class Lesson05Test {
 
-    @Test
-    public void test() {
-        System.out.println("The property: ");
+    @Test(enabled = false)
+    public void propertyTest() {
+//        Не работает
+        System.out.println("The property: " + PropertyReader.getPropertyFromFile("properties/settings.properties", "timeout"));
     }
 }
